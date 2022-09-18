67a37ec5ee4c692ab333f0b723f7076eefbbd48b
jojutsen <109443721+jojutsen@users.noreply.github.com>
2022-09-18 20:16:22 +0700

    Update viewbot.py

--- viewbot.py
+++ viewbot.py
@@ -257,7 +257,7 @@ class Main:
 
                 if "Too many requests. Please slow down." in decrypted_response:
                     print(self.format("x", "Ratelimited"))
-                    time.sleep(20)
+                    time.sleep(120)
                     continue
 
                 timer = self.decrypt_timer(decrypted_response)
